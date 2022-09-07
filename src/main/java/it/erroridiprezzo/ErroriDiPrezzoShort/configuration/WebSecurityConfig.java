47d23bb5fb764c35e5175594fb238b4da72e69d3
Gianluca Graziadei <graziadei.gianluca@gmail.com>
2022-09-07 08:44:24 +0200

    General fixes

--- src/main/java/it/erroridiprezzo/ErroriDiPrezzoShort/configuration/WebSecurityConfig.java
+++ src/main/java/it/erroridiprezzo/ErroriDiPrezzoShort/configuration/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package it.erroridiprezzo.ErroriDiPrezzoShort.configuration;
 
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -10,12 +11,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests()
+       /* http.authorizeHttpRequests()
                 .antMatchers("/admin*").authenticated()
-                .antMatchers("/short").authenticated()
+                .antMatchers(HttpMethod.POST , "/short").authenticated()
                 .antMatchers("/").permitAll()
                 .and().formLogin()
                 .and().httpBasic();
+        */
+
+        http
+                .csrf().disable()
+                .authorizeRequests().anyRequest().authenticated()
+                .and()
+                .httpBasic()
+                .and().formLogin();
+
     }
 
 }
